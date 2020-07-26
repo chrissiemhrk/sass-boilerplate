7127e9a07a85562d3a7ec9d15778c85d8d2db981
Chrissie Muhorakeye <chrissiemhrk@gmail.com>
2020-07-26 08:13:30 +0200

    add: browser sync setup

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,23 @@ const browserSync = require("browser-sync").create();
 function style() {
   return gulp
     .src("./src/scss/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./src/css"));
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./src/css"))
+    .pipe(browserSync.stream());
+}
+
+// browsersync setup
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./src/",
+    },
+  });
+  gulp.watch("./src/scss/**/*.scss", style);
+  gulp.watch("./src/*.html").on("change", browserSync.reload);
+  gulp.watch("./src/js/**/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
